b837526f0011e533269c7ad79b0a835c3d24773e
Tim <timjanssenj@gmail.com>
2025-06-09 12:30:26 +0200

    Revert "Merge branch 'Experiment_1_merged' into Experiment_2"
    This reverts commit 1744b60d12d15d765445883b4ee4253c2d380f9e, reversing
    changes made to a4a16970f70d30550481cf3f5d7873c9a7b1ab40.

--- app.py
+++ app.py
@@ -7,12 +7,9 @@ import solara
 import mesa
 from structure import Structure
 
-print("Mesa version:", mesa.__version__)
+print(mesa.__version__)
 
-# Initialize model
-niche_model = NicheModel(n_agents=20, max_resource=2, mutation_rate=0.01, recharge_rate=0.25)
 
-# Agent appearance logic
 def agent_portrayal(agent):
     if isinstance(agent, Tile):
         x, y = agent.pos
@@ -20,8 +17,8 @@ def agent_portrayal(agent):
         norm = min(val / agent.model.max_resource, 1.0)
 
         return {
-            "color": (norm, norm, norm),  # grayscale for environment level
-            "size": 40 # fill cell
+            "color": (norm, norm, norm),
+            "size": 40  # fill cell
         }
 
     if isinstance(agent, Structure):
@@ -41,7 +38,6 @@ def agent_portrayal(agent):
 
     return None
 
-# User-configurable model parameters
 model_params = {
     "n_agents": {"type": "SliderInt", "min": 5, "max": 100, "value": 20, "label": "Initial Population"},
     "mutation_rate": {"type": "SliderFloat", "min": 0.0, "max": 0.5, "value": 0.01, "step": 0.01, "label": "Mutation Rate"},
@@ -49,7 +45,8 @@ model_params = {
     "recharge_rate": {"type": "SliderFloat", "min": 0.0, "max": 1.0, "value": 0.25, "step": 0.05, "label": "recharge rate of resource per tile"},
 }
 
-# Space and plots          
+niche_model = NicheModel(n_agents=20, max_resource=2, mutation_rate=0.01, recharge_rate=0.25)
+
 space_component = make_space_component(agent_portrayal)
 resource_plot = make_plot_component("MeanResource", backend="matplotlib")
 energy_plot = make_plot_component("MeanEnergy", backend="matplotlib")
@@ -60,16 +57,11 @@ plot_dna_4 = make_plot_component("MeanBuilder", backend="matplotlib")
 plot_dna_5 = make_plot_component("MeanMovement", backend="matplotlib")
 plot_population = make_plot_component("OrganismCount", backend="matplotlib")
 plot_structures = make_plot_component("StructureCount", backend="matplotlib")
-plot_age = make_plot_component("AvgLifespan", backend="matplotlib")          
 
-# Interactive web app
 page = SolaraViz(
     niche_model,
     components=[space_component, resource_plot, energy_plot, plot_dna_1, plot_dna_2, plot_dna_3, 
-                plot_dna_4, plot_dna_5, plot_population, plot_structures, plot_age],
+                plot_dna_4, plot_dna_5, plot_population, plot_structures],
     model_params=model_params,
-    name="Niche Construction Model: First Experiment, Local Environmental Modification"
+    name="Niche Construction Model"
 )
-
-#page
-
